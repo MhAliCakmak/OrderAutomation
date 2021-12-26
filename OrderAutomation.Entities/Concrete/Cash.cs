870e47f8fe2f7cebd04e0d8306d080edc2c15539
Mehmet ali <79807829+mehmet5643@users.noreply.github.com>
2021-12-26 22:52:39 +0300

    Entities loaded

---
+++ OrderAutomation.Entities/Concrete/Cash.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderAutomation.Entities.Concrete
+{
+    public class Cash 
+    {
+        public int CashId { get; set; }
+        public double CashTendered { get; set; }
+
+    }
+}
---
+++ OrderAutomation.Entities/Concrete/Check.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OrderAutomation.Entities.Concrete
+{
+    public class Check : Payment
+    {
+        public int CheckId { get; set; }
+        public string Name { get; set; }
+        public string BankID { get; set; }
+        public override decimal Amount { get; set; }
+
+    }
+}
